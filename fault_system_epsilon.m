7229a3fc0962b2a9d2072d904a6c1793f56da6bb
brendanjmeade <brendanjmeade@users.noreply.github.com>
2020-10-19 23:56:44 -0400

    Euler integration to function -> staging for parameter experiments.

---
+++ fault_system_epsilon.m
@@ -0,0 +1,83 @@
+function fault_system_epsilon()
+% Calculates displacements along 2 nearly parallel fault strands
+% given some idealized assumptions:
+% stress ~ sigma_xy + epsilon(t)
+% fault1 ~ yield stress tau0, angle ~ 0 + epsilon1
+% fault2 ~ yield stress tau0, angle ~ 0 + epsilon2
+% Instead of changes directly to sigma_xy, below implements changes to
+% principal stress angle as the perturbation
+% yield criterion sigma_xy >= tau0
+% System driven by applied constant velocity
+% Stress is calculated elastically
+% Written by VCT
+    sigma_xy = 1;
+    tau0 = 1;
+    v = 1;
+    mu = 1;
+    H = 1;
+    dt = 0.001;
+    t = [0:dt:10];
+
+    % To toggle epsilon1 angle or ps_angle, pick different flags
+    eps1_flag = 1;
+    ps_angle_flag = 1;
+
+    if eps1_flag == 1
+        epsilon1 = 0.05;
+    elseif eps1_flag == 2
+        epsilon1 = 0.02;
+    else
+        epsilon1 = 0;
+    end
+    epsilon2 = 0;
+    if ps_angle_flag == 1
+        ps_angle = 0.1 * cos(2 * t); 
+    else
+        ps_angle = 0 * t;
+    end
+        
+    [dd, dd1, dd2] = euler_integrate(t, v, [0, 0, 0], epsilon1, epsilon2, ...
+                                     tau0, ps_angle, mu, H);
+    
+    figure;
+    hold on;
+    plot(t, dd, "-k")
+    plot(t, dd1, "-r")
+    plot(t, dd2, "-b")
+    xlabel("time");
+    box on;
+    legend("d", "d1", "d2");    
+end
+
+
+function [d, d1, d2] = euler_integrate(t, v, ics, epsilon1, epsilon2, ...
+                                       tau0, ps_angle, mu, H)
+    dt = t(2) - t(1);
+    d = v * t;
+    d1 = zeros(size(t));
+    d2 = zeros(size(t));
+            
+    for i=2:length(t)
+        sigma_xy = mu / H * (d(i-1) - d1(i-1) - d2(i-1));
+        sigma_shear1(i) = sigma_xy * cos(ps_angle(i-1) + epsilon1);
+        sigma_shear2(i) = sigma_xy * cos(ps_angle(i-1) + epsilon2);
+        if sigma_shear1(i) >= tau0 && sigma_shear2(i) < tau0
+            %increment fault1
+            d1(i) = d1(i-1) + v * dt;
+            d2(i) = d2(i-1);
+        elseif sigma_shear1(i) < tau0 && sigma_shear2(i) >= tau0
+            %increment fault2
+            d1(i) = d1(i-1);
+            d2(i) = d2(i-1) + v * dt;
+        elseif sigma_shear1(i) < tau0 && sigma_shear2(i) < tau0
+            %neither fault incremented
+            d1(i) = d1(i-1);
+            d2(i) = d2(i-1);
+        else
+            %split the increment with a 50% rule
+            d1(i) = d1(i-1) + 0.5 * v * dt;
+            d2(i) = d2(i-1) + 0.5 * v * dt;
+        end
+    end
+end
+
